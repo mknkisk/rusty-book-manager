c03c91053157f2543ba6942a55a9c3359961971e
mknkisk <mknkisk@gmail.com>
2024-12-15 16:46:50 +0900

    5.2.6 シンプルな蔵書データの登録・取得処理の作成 - registry モジュールの実装

--- registry/src/lib.rs
+++ registry/src/lib.rs
@@ -1,24 +1,31 @@
 use std::sync::Arc;
 
-use adapter::{database::ConnectionPool, repository::health::HealthCheckRepositoryImpl};
+use adapter::{database::ConnectionPool, repository::{book::BookRepositoryImpl, health::HealthCheckRepositoryImpl}};
 use kernel::repository::health::HealthCheckRepository;
 
 #[derive(Clone)]
 pub struct AppRegistry {
     health_check_repository: Arc<HealthCheckRepositoryImpl>,
+    book_repository: Arc<BookRepositoryImpl>,
 }
 
 impl AppRegistry {
     pub fn new(pool: ConnectionPool) -> Self {
         // 依存解決。derive-new を使わずにコンストラクタの内部実装を手書きする
-        let health_check_repository = Arc::new(HealthCheckRepositoryImpl::new(pool));
+        let health_check_repository = Arc::new(HealthCheckRepositoryImpl::new(pool.clone()));
+        let book_repository = Arc::new(BookRepositoryImpl::new(pool.clone()));
         Self {
             health_check_repository,
+            book_repository,
         }
     }
 
     // 依存解決したインスタンスを返すメソッド
     pub fn health_check_repository(&self) -> Arc<dyn HealthCheckRepository> {
         self.health_check_repository.clone()
+    }
+
+    pub fn book_repository(&self) -> Arc<BookRepositoryImpl> {
+        self.book_repository.clone()
     }
 }
